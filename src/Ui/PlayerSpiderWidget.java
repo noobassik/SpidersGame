2c375046a366c42b078325cca069658190f2284a
David Sandrosyan <david-010803@mail.ru>
2024-05-28 21:43:38 +0300

    changed spider movement logic by key

--- src/Ui/PlayerSpiderWidget.java
+++ src/Ui/PlayerSpiderWidget.java
@@ -43,14 +43,15 @@ public class PlayerSpiderWidget extends AnimalWidget {
 
         @Override
         public void keyPressed(KeyEvent ke) {
-            int keyCode = ke.getKeyCode();
 
-            moveAction(keyCode);
 
         }
 
         @Override
-        public void keyReleased(KeyEvent arg0) {
+        public void keyReleased(KeyEvent ke) {
+            int keyCode = ke.getKeyCode();
+
+            moveAction(keyCode);
         }
 
         private void moveAction(int keyCode) {
